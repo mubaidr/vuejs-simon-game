48fd3fbe9fb31a04120775efe8424b71b338ba25
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-08-31 22:14:56 +0500

    intial files

---
+++ docs/index.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line
+Vue.config.productionTip = false
+// eslint-disable-next-line
+var myApp = new Vue({
+  el: '#app',
+  data: {},
+  created () {},
+  mounted () {},
+  watch: {},
+  computed: {},
+  methods: {}
+})
